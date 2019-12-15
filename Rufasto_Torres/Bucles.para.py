22b29a50f7d97c607ab5c4d08a7dd54f7fa27d21
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-15 18:00:30 -0500

    Se agrego el 2do ejercicio de bucles para

--- Rufasto_Torres/Bucles.para.py
+++ Rufasto_Torres/Bucles.para.py
@@ -5,3 +5,14 @@ while(n<102):
     print(n)
     n+=2
 #fin_while
+
+
+#Ejercicio02
+#Mostrar la suma de los 50 primeros numeros
+n=0
+s=0
+while(n<=50):
+    n=n+1
+    s=s+n
+#fin_while
+print("Suma:",s)
