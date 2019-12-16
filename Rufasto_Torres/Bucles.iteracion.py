e01cac449bd38e0ed2436fde29c9ff840ed5746f
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-16 17:54:54 -0500

    Se agrego el 1er ejercicio de iteracion

---
+++ Rufasto_Torres/Bucles.iteracion.py
@@ -0,0 +1,13 @@
+#Ejercicio01
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="&"):
+        print("llamame ahorita")
+    if(letra=="#"):
+        print("llamame en 5 min")
+    if(letra=="@"):
+        print("ya no me llames")
+    if(letra=="/"):
+        print("donde estas")
+#fin_for
---
+++ Rufasto_Torres/lote.bat
@@ -0,0 +1,5 @@
+Python Rufasto_Torres/Bucles.iteracion.py
+&= llamame ahorita
+#= llamame en 5 min
+@= ya no me llames
+/= donde estas
