13c4a0e85222e1ba0a99e2af13ff80f33e54c5a2
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-16 18:57:37 -0500

    Se agrego los bucles para de llontop

---
+++ Llontop_Atencio/Bucles.para.llontop.py
@@ -0,0 +1,55 @@
+#Ejercicio01
+i=0
+s=0
+max=50
+while(i<=max):
+    i+=1
+    if((i%2)==0):
+        s=s+2
+        #fin_si
+#fin_while
+print("suma pares:",s)
+
+
+
+#Ejercicio02
+#Numeros pares del 0 al 100
+n=0
+while(n<=100):
+    print(n)
+    n+=2
+#fin_while
+
+
+#Ejercicio03
+#suma de la serie
+i=0
+s=0
+max=30
+while(i<=max):
+    i+=3
+    if((i%2)==0):
+        s=s+10
+        #fin_si
+#fin_while
+print("suma:",s)
+
+
+
+#Ejercicio04
+#numeros del 10 al 40
+n=10
+while(n<=40):
+    print(n)
+    n+=1
+#fin_while
+
+
+
+#Ejercicio05
+#numeros del 150 al 50
+i=150
+while(i>=50):
+    print(i)
+    i-=1
+#fin_while
