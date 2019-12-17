51e53794b66e7b3527fbf9c9d18c2ece95af4839
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-16 19:02:19 -0500

    Se agrego los bucles iteracion de llontop

---
+++ Llontop_Atencio/Bucles.iteracion.llontop.py
@@ -0,0 +1,65 @@
+#Ejercicio01
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="A"):
+        print("felicitaciones")
+    if(letra=="B"):
+        print("muy bien")
+    if(letra=="C"):
+        print("bien")
+#fin_for
+
+
+#Ejercicio02
+import  os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="U"):
+        print("mamá")
+    if(letra=="E"):
+        print("abreme la puerta")
+    if(letra=="I"):
+        print("necesito dinero")
+    if(letra=="O"):
+        print("te amo")
+#fin_for
+
+
+#Ejercicio03
+import os
+msg=os.sys.argv[1]
+for numero in msg:
+    if(numero=="1"):
+        print("gracias por su compra")
+    if(numero=="2"):
+        print("lo esperamos pronto")
+    if(numero=="3"):
+        print("gano un vale de 100 soles")
+#fin_for
+
+
+#Ejercicio04
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="x"):
+        print("peligro")
+    if(letra=="y"):
+        print("cuidado")
+    if(letra=="z"):
+        print("despacio")
+#fin_for
+
+
+#Ejercicio05
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    if(letra=="L"):
+        print("llamame urgente")
+    if(letra=="J"):
+        print("estoy en clase")
+    if(letra=="K"):
+        print("dejame tus llaves")
+#fin_for
